a1438b71bcf32e2bc98ae3bc4e2d4802869da8aa
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-22 01:02:38 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,8 @@
 def alphabetize(arr)
   # code here
   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by do |word|
-    word.split(//).collect do |letter|
+  arr.sort_by do |words|
+    words.split("").collect do |letter|
       esperanto.index(letter)
     end
   end
